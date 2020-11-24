64d23206118c0da56e5a4ee0dfe499099318de4f
Quentin Dstl <qdesautel@gmail.com>
2020-11-24 23:40:00 +0100

    Mise en place projet
    premier commit

---
+++ src/java_project_desautel_pellen_perold/Java_project_desautel_pellen_perold.java
@@ -0,0 +1,8 @@
+package java_project_desautel_pellen_perold;
+
+public class Java_project_desautel_pellen_perold {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
