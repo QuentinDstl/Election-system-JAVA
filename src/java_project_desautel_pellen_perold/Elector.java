2b9533f173313f7d601138e09b6eb96997681b62
clementpellen <clementpellen@gmail.com>
2020-11-27 17:45:44 +0100

    implémentation

--- src/java_project_desautel_pellen_perold/Elector.java
+++ src/java_project_desautel_pellen_perold/Elector.java
@@ -1,5 +1,40 @@
 package java_project_desautel_pellen_perold;
 
-public class Elector {
+public class Elector extends Personne {
     
+    /* Variables */
+    State m_state;
+    Candidate m_candidate;
+    boolean m_vote_done;
+    
+    
+    /* Constructor */
+    public Elector() {}
+    
+    /* Getters */
+    public State getState() {
+        return m_state;
+    }
+    
+    public String getNameState() {
+        return m_state.getName;
+    }
+    
+    public Candidate getCandidate() {
+        return m_candidate;
+    }
+    
+    public String getNameCandidate() {
+        return m_candidate.getName();
+    }
+    
+    public boolean has_already_voted() {
+        return m_vote_done;
+    }
+    
+    
+    /* Setters */
+    public void is_voting() {
+        m_vote_done = true;
+    }
 }
--- src/java_project_desautel_pellen_perold/Personne.java
+++ src/java_project_desautel_pellen_perold/Personne.java
@@ -2,4 +2,25 @@ package java_project_desautel_pellen_perold;
 
 public class Personne {
     
+    /* Variables */
+    private String m_first_name;
+    private String m_last_name;
+    private String m_password;
+    
+    
+    /* Constructor */
+    public Personne() {}
+    
+    /* Getter */
+    public String getFirstName() {
+        return  m_first_name;
+    }
+    
+    public String getLastName() {
+        return m_last_name;
+    }
+    
+    public String getPassword() {
+        return m_password;
+    }
 }
