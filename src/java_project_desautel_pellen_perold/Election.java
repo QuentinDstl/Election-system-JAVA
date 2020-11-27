cb7d3f3c45b2dcf7605cf175882b6da0dc9308bb
Quentin Dstl <qdesautel@gmail.com>
2020-11-27 17:45:39 +0100

    Election modif et ajout UML & README

--- src/java_project_desautel_pellen_perold/Election.java
+++ src/java_project_desautel_pellen_perold/Election.java
@@ -1,5 +1,37 @@
 package java_project_desautel_pellen_perold;
 
+import java.util.ArrayList;
+
 public class Election {
+    private boolean m_openVote;
+    private ArrayList<State> m_states;
+
+    public Election(boolean openVote) {
+        m_openVote = openVote;
+        m_states = new ArrayList<>();
+    }
+
+    public boolean getOpenVote() {
+        return m_openVote;
+    }
+
+    public void setOpenVote(boolean openVote) {
+        m_openVote = openVote;
+    }
     
+    public void addState(State state) throws NullPointerException {
+        if(state == null) {
+            throw new NullPointerException("dans addState de la class Election : le state est null");
+        }  
+        else
+            m_states.add(state);
+    }
+    
+    public void showVoteNational() {
+        // Appeler Vue
+    }
+    
+    public void showWinner() {
+        // Appeler Vue
+    }
 }
--- src/java_project_desautel_pellen_perold/Java_project_desautel_pellen_perold.java
+++ src/java_project_desautel_pellen_perold/Java_project_desautel_pellen_perold.java
@@ -3,6 +3,6 @@ package java_project_desautel_pellen_perold;
 public class Java_project_desautel_pellen_perold {
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
 }
