bbd564232e6c22507c3303c17a39a9cc82e17219
clementpellen <clementpellen@gmail.com>
2020-12-06 12:03:00 +0100

    Calculs des scores des candidats

--- src/view_package/Controller.java
+++ src/view_package/Controller.java
@@ -5,6 +5,7 @@ import dao_package.CandidateDAOImpl;
 import dao_package.ElectorDAOImpl;
 import dao_package.OfficialDAOImpl;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import javax.swing.*;
 import java_project_desautel_pellen_perold.*;
 
@@ -400,5 +401,25 @@ public class Controller {
         return  m_access_to_candidate_table.checkUserCandidateName(last_name, first_name) ||
                 m_access_to_official_table.checkUserOfficialName(last_name, first_name)||
                 m_access_to_elector_table.checkUserElectorName(last_name, first_name);
+    }
+    
+    private String getWinnerOfState(State state) {
+        String name_winner = "";
+        int nb_votes_winner = 0;
+        for(int i=0; i<m_access_to_election.getCandidates().size(); ++i) {
+            if(state.getNbVotesCandidateInState(m_access_to_election.getCandidates().get(i).getLastName()) > nb_votes_winner) {
+                nb_votes_winner = state.getNbVotesCandidateInState(m_access_to_election.getCandidates().get(i).getLastName());
+                name_winner = m_access_to_election.getCandidates().get(i).getLastName();
+            }
+        }
+        return name_winner;
+    }
+    
+    private ArrayList<Integer> getProportionnalityCandidatesOfState(State state) {
+        ArrayList<Integer> candidates_scores = new ArrayList<>();
+        for(int i=0; i<m_access_to_election.getCandidates().size(); ++i) {
+            candidates_scores.add(state.getNbVotesCandidateInState(m_access_to_election.getCandidates().get(i).getLastName()) / m_access_to_election.getCandidates().size());
+        }
+        return candidates_scores;
     }
 }
