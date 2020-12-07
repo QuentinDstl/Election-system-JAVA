bfb243a7fca10f77b4cb4a87f474e9dfdcdeff85
Charles P <c_perold@hotmail.fr>
2020-12-07 16:16:30 +0100

    Suppression texte inutile

--- src/java_project_desautel_pellen_perold/Java_project_desautel_pellen_perold.java
+++ src/java_project_desautel_pellen_perold/Java_project_desautel_pellen_perold.java
@@ -1,26 +1,16 @@
 package java_project_desautel_pellen_perold;
 
-//import dao_package.*;
 import config_package.Log;
 import static config_package.Start.start;
 import view_package.*;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Java_project_desautel_pellen_perold {
     
-    public  static void main(String[] args) throws SQLException
+    public static void main(String[] args) throws SQLException
     {
-       // Candidate candidate = new Candidate(1);
-
-        Log log = new Log();
-        /* start("name_config") -> to load with your config but chose the file to open */
-        /* start("name_config","file_path") -> to load with yout config and the file */
-        /* start() -> the program will ask you everything and save it in the config file */
-        start();              // une fois que vous avez utilise ca vous pouvez juste utiliser start() ce sera sauvegarde
-        
-        //yo.createTableElector();
-        //yo.addCandidate("Dého", "qd", "puceauLand");
-        
+        initialization();
 
         /* PARTIE GRAPHIQUE EN TEST */
         Election access_to_election = new Election();
@@ -47,5 +37,24 @@ public class Java_project_desautel_pellen_perold {
             }
         } while (resetOut == 0);
         System.exit(0);
+    }
+    
+    public static void initialization() {
+
+        /* We init the logs.txt file in the project folder */
+        Log log = new Log();
+        
+        System.out.println("Do you want to charge a different xlsx file ? (yes or no)");
+        Scanner scanner = new Scanner(System.in);
+        
+        /* start("name_config") -> to load with your config but chose the file to open */
+        /* start("name_config","file_path") -> to load with yout config and the file */
+        /* start() -> the program will ask you everything and save it in the config file */
+        /* if the config file have already saved your data it will not ask you */
+        
+        if(scanner.nextLine().equals("yes"))
+            start("default");
+        else
+            start();
     }
 }
